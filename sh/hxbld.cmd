f9d8ff61f0249cccadd9bbe56eb49ea7cda9c2de
John O'Sullivan <osullivj@gmail.com>
2023-04-14 11:46:36 +0100

    hxbld changes; index.html now in hx and favicon.ico in icons.

--- sh/hxbld.cmd
+++ sh/hxbld.cmd
@@ -1,4 +1,6 @@
 @echo off
 if -%BDROOT%-==-- echo BDROOT env var must be set & exit /b
 cd %BDROOT%
+copy %BDROOT%\src\hx\index.html %BDROOT%\html
+copy %BDROOT%\icons\favicon.ico %BDROOT%\html
 haxe --verbose --dce full --class-path %BDROOT%\src\hx html5.hxml

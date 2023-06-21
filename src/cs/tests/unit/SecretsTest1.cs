6de0534e5bba9e6a008b6e81f6d1394a34f27588
John O'Sullivan <osullivj@gmail.com>
2023-06-21 11:47:12 +0100

    boilerplate for secrets unit test

---
+++ src/cs/tests/unit/SecretsTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using BizDeck;
+
+namespace BizDeckUnitTests {
+    public class SecretsTest1 {
+        [SetUp]
+        public void Setup() {
+            // TODO: need and alternate cfg dir for test config
+            // probably need alt dir tree for test scripts too
+            // two secrets tests: HTTP construction, secret sub in tiingo_gui_login
+            string[] args = { "--appdata", TestContext.Parameters.Get("appdata", "c:\\osullivj\\src") };
+            CmdLineOptions.InitAndLoadConfigHelper(args);
+        }
+
+        [Test]
+        public void TestPlayScriptSecrets() {
+            var bdconfig = ConfigHelper.Instance.BizDeckConfig;
+            // TODO add code that loads tiingo_gui_login and validates
+            // subs against test secrets cfg
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestHTTPSecrets() {
+            var bdconfig = ConfigHelper.Instance.BizDeckConfig;
+            // TODO add code that loads quandl_rates and checks that
+            // the URLs are decorated with auth keys correctly
+            Assert.Pass();
+        }
+    }
+}

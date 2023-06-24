8fad5cb725c143c377f2aa635ef9b8c4a1cbee94
John O'Sullivan <osullivj@gmail.com>
2023-06-24 10:27:29 +0100

    cpy 3.11 venv for ansible and int testing

--- sh/droot.bat
+++ sh/droot.bat
@@ -2,6 +2,7 @@
 set BDROOT=C:\osullivj\src\BizDeck
 set HXROOT=C:\osullivj\bld\HaxeToolkit
 set IRONPYTHON=c:\osullivj\bin\irnpy340\ipy.exe
+set CPYTHON=c:\osullivj\bin\py311x64\python.exe
 :: you should not need to change the lines below
 set IRONPYTHONPATH=%BDROOT%\src\py\core;%BDROOT%\src\py\mock
 set PATH=%PATH%;%HXROOT%\haxe;%HXROOT%\neko
---
+++ sh/venv.bat
@@ -0,0 +1,3 @@
+%CPYTHON% -m venv %BDROOT%\venv
+%BDROOT%\venv\scripts\pip.exe install --upgrade pip
+%BDROOT%\venv\scripts\pip.exe install -r %BDROOT%\cpy_requirements.txt
